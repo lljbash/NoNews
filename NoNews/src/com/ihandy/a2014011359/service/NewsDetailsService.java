3ad553bc9410310763baa702203d6a550ebb7e22
lljbash <llj_95@163.com>
2016-09-09 20:08:51 +0800

    now NewsDetailsService uses HttpRequest instead of URL

--- NoNews/src/com/ihandy/a2014011359/service/NewsDetailsService.java
+++ NoNews/src/com/ihandy/a2014011359/service/NewsDetailsService.java
@@ -12,19 +12,32 @@ package com.ihandy.a2014011359.service;
         import android.text.TextUtils;
         import android.util.Log;
 
+        import com.ihandy.a2014011359.tool.HttpRequest;
+
 public class NewsDetailsService {
     public static String getNewsDetails(String url, String news_title,
                                         String news_date) {
         String data = "";
         try {
-            URL cs = new URL(url);
+            /*URL cs = new URL(url);
             Log.i("URL", url);
             BufferedReader in = new BufferedReader(new InputStreamReader(cs.openStream()));
             String inputLine;
             while ((inputLine = in.readLine()) != null){
                 data += inputLine;
-            }
-        } catch (IOException e) {
+            }*/
+            HttpRequest request = HttpRequest.get(url);
+            String body = request.body();
+//            int start = body.indexOf("<section class=\"article\">");
+//            if (start != -1) {
+//                int end = body.indexOf("</section>", start);
+//                Log.i("<body>", start + ", " + end);
+//                data = body.substring(start, end + 10);
+//            }
+//            else {
+                data = body;
+//            }
+        } catch (HttpRequest.HttpRequestException e) {
             e.printStackTrace();
         }
         return data;
